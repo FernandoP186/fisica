e4b6e013118817e3b2bd2acee7b91a8a67d926f3
FernandoP186 <93397593+FernandoP186@users.noreply.github.com>
2021-11-04 12:27:38 -0300

    change2.0

--- change.js
+++ change.js
@@ -1,56 +1,56 @@
 function work(x)
-    {
-        var art_id = ['objetivo', 'material', 'resultado', 'referencia', 'gas']
-        var obj_id = ['fir','sec','thi','fou','fiv']
-        var c_color = 'rgba(255, 255, 255, 0)'
+{
+    var art_id = ['objetivo', 'material', 'resultado', 'referencia', 'gas']
+    var obj_id = ['fir','sec','thi','fou','fiv']
+    var c_color = 'rgba(255, 255, 255, 0)'
 
-        function test(x)
+    function test(x)
+    {
+        for (var num = 0; num != 6; num++)
         {
-            for (var num = 0; num != 6; num++)
+            if (num == x)
             {
-                if (num == x)
-                {
-                    document.getElementById(obj_id[num]).style.backgroundColor = 'black'
-            
-                }
-                else
-                {
-                    document.getElementById(obj_id[num]).style.backgroundColor = c_color
-                }
+                document.getElementById(obj_id[num]).style.backgroundColor = 'black'
+        
             }
-            
-            for (var num = 0; num != 6; num++)
+            else
             {
-                if (num == x)
-                {
-                    document.getElementById(art_id[num]).style.display = 'block'
-                }
-                else
-                {
-                    document.getElementById(art_id[num]).style.display = 'none'
-                }
+                document.getElementById(obj_id[num]).style.backgroundColor = c_color
             }
         }
         
-        if (x == 0)
+        for (var num = 0; num != 6; num++)
         {
-            test(x)
+            if (num == x)
+            {
+                document.getElementById(art_id[num]).style.display = 'block'
+            }
+            else
+            {
+                document.getElementById(art_id[num]).style.display = 'none'
+            }
         }
-        else if (x == 1)
-        {
-            test(x)
-        }
-        else if (x == 2)
-        {
-            test(x)
-        }
-        else if (x == 3)
-        {
-            test(x)
-        }
-        else if (x == 4)
-        {
-            test(x)
-        }
-        
-    } 
+    }
+    
+    if (x == 0)
+    {
+        test(x)
+    }
+    else if (x == 1)
+    {
+        test(x)
+    }
+    else if (x == 2)
+    {
+        test(x)
+    }
+    else if (x == 3)
+    {
+        test(x)
+    }
+    else if (x == 4)
+    {
+        test(x)
+    }
+    
+} 
